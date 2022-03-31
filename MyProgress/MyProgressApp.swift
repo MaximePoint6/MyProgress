80b0ff93b5fd6dd4c5557f7e1983e7ebcee11929
Maxime Point <maximepoint6@gmail.com>
2022-03-31 13:43:47 +0200

    Initial Commit

---
+++ MyProgress/MyProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProgressApp.swift
+//  MyProgress
+//
+//  Created by Maxime Point on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
